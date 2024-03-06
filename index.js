4259ae5ad5bef661cc929e04a7618174588c4e1d
JonKern <jon.alex.kern@gmail.com>
2024-03-05 18:47:57 -0600

    Add POST route and use user selected genre as param. Display show info in index.ejs

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 import express from "express";
 import axios from "axios";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+const API_URL = "https://api.jikan.moe/v4/";
 
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", async (req, res) => {
   res.render("index.ejs");
+});
+
+app.post("/results", async (req, res) => {
+  try {
+    let userGenres = req.body.genres;
+    const response = await axios.get(API_URL + "anime", {
+      params: {
+        sfw: true,
+        type: "tv",
+        min_score: 8.0,
+        genres: userGenres,
+        limit: 5,
+      },
+    });
+    const result = response.data;
+
+    console.log(result.data[0].title);
+
+    res.render("index.ejs", {
+      shows: result.data,
+    });
+  } catch (error) {
+    console.error("failed to make request:", error.message);
+    console.log(error.response);
+  }
 });
 
 app.listen(port, () => {
